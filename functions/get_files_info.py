cedd0fbf6df4405706cdd5d99cd215106552c7a1
Milkstach3 <charleswletcher@gmail.com>
2025-07-01 16:30:03 -0500

    first commit

---
+++ functions/get_files_info.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+def get_files_info(working_directory, directory=None):
+    # Build and return a string representing the contents of the directory. It should use this format:
+    # - <filename>: file_size=<size> bytes, is_dir=<True/False>
+    # - README.md: file_size=1032 bytes, is_dir=False
+    # - src: file_size=128 bytes, is_dir=True
+    # - package.json: file_size=1234 bytes, is_dir=False
+
+    
+    print("Started trying")
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = abs_working_dir
+
+    if directory:
+        target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(target_dir):
+        print(f'Error: "{directory}" is not a directory')
+        return exit(1)
+    
+    if not directory in working_directory:
+        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        return exit(1)
+
+ 
+    try:    
+        # files_info is where we will store the information about each file
+        files_info = []
+        # os.listdir returns an iterable list of the files in the directory. filename is an individual file.
+        for filename in os.listdir(target_dir):
+            # filepath is value passed to isdir and getsize. Joining target_dir and filename puts the value in the correct format.
+            filepath = os.path.join(target_dir, filename)
+            # Initialize file_size to 0 so Python knows it's an integer.
+            file_size = 0
+            # Check if the filepath is a directory or a file
+            is_dir = os.path.isdir(filepath)
+            # Get the file size.
+            file_size = os.path.getsize(filepath)
+            # Add the filename, file_size and is_dir to the formatted string, and add the formatted string to the end of the files_info array.
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        # Concatenate all the strings of files_info into a single string with newlines.
+        return "\n".join(files_info)
+    
+        # os.path.abspath(): Get an absolute path from a relative path
+        # os.path.join(): Join two paths together safely (handles slashes)
+        # .startswith(): Check if a string starts with a substring
+        # os.path.isdir(): Check if a path is a directory
+        # os.listdir(): List the contents of a directory
+        # os.path.getsize(): Get the size of a file
+        # os.path.isfile(): Check if a path is a file
+        # .join(): Join a list of strings together with a separator
+
+    
+    except Exception as e:
+        return f"Error listing files: {e}"
+    
+
+
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    verbose = False
+    if sys.argv[-1] == "--verbose":
+        sys.argv.pop()
+        verbose = True
+
+    if len(sys.argv) <= 1:
+        print("Usage: python main.py <prompt>")
+        return exit(1)
+    
+
+
+    user_prompt = " ".join(sys.argv[1:])
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages 
+    )
+
+    print(response.text)
+
+    if verbose:
+        print("User prompt: " + user_prompt)
+        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+
+main()
